cccc915ff556ad1c291ccca4b3128c9fa9a22254
bethelcolonyit <it@bethelcolony.org>
2025-12-02 10:18:12 -0500

    enable cors from frontend

--- server/index.js
+++ server/index.js
@@ -95,7 +95,10 @@ app.use(express.static('public'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:4200',  // your Angular dev origin
+  credentials: true,               // allow cookies to be sent
+}));
 
 
 function ensureAuthenticated(req, res, next) {
