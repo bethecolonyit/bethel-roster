3548af6b4b4f9ecf7fe66e20e4e6fb90cff2e8d4
bethelcolonyit <it@bethelcolony.org>
2025-12-10 00:18:35 -0500

    building out counseling coordinator dashboard

--- client/src/app/components/departments/c-coord/c-coord-dashboard/c-coord-dashboard.ts
+++ client/src/app/components/departments/c-coord/c-coord-dashboard/c-coord-dashboard.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
-
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { StudentService } from '../../../../services/student.service';
+import { Student } from '../../../../models/student';
+import { CommonModule } from '@angular/common';
 @Component({
-  selector: 'app-c-coord-dashboard',
-  imports: [],
+  selector: 'app-counseling-dashboard',
+  imports: [CommonModule, MatCardModule, MatMenuModule, MatIconModule],
   templateUrl: './c-coord-dashboard.html',
   styleUrl: './c-coord-dashboard.scss',
+  standalone: true,
 })
 export class CCoordDashboard {
+
+  students: Student[] = [];
+  studentsNeedingCounselor: Student[] = [];
+  filteredStudents: Student[] = []
+  error: string | null = null;
+
+  constructor(private studentService: StudentService, private cdr: ChangeDetectorRef) {
+    this.loadStudents();
+
+  }
+
+  loadStudents() {
+    this.studentService.getStudents().subscribe({
+      next: (data) => { 
+        this.students = data; 
+        this.filteredStudents = [...this.students];
+        this.loadStudentsNeedingCounselor();
+        this.cdr.detectChanges(); 
+      },
+      error: () => this.error = 'Error loading students'
+    });
+  }
+
+  loadStudentsNeedingCounselor() {
+    this.studentsNeedingCounselor = this.students.filter(student => !student.counselor);
+  }
+
+  emptyFunction() { } // Placeholder for future functionality
+  onAssignCounselor(student: Student) { } // Placeholder for future functionality 
 
 }
--- client/src/app/components/departments/counseling/counseling-dashboard/counseling-dashboard.ts
+++ client/src/app/components/departments/counseling/counseling-dashboard/counseling-dashboard.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { Student } from '../../../../models/student';
 @Component({
   selector: 'app-counseling-dashboard',
-  imports: [],
+  imports: [MatCardModule, MatMenuModule, MatIconModule],
   templateUrl: './counseling-dashboard.html',
   styleUrl: './counseling-dashboard.scss',
+  standalone: true,
 })
 export class CounselingDashboard {
+
+  students: Student[] = [];
+
+  emptyFunction() { } // Placeholder for future functionality
 
 }
